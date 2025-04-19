f388ac607b193d56e89340f207d6b3c8a224a1f4
urvashi <urvashikshatriya9@gmail.com>
2025-04-19 10:20:07 +0530

    learned the basic concept of prop drilling and component rendering

--- basic_react/src/App.jsx
+++ basic_react/src/App.jsx
@@ -1,9 +1,29 @@
-
+import { useState } from "react"
 function App() {
   
+  let [counter,setCounter] = useState(15)
 
+  const addValue = () => {
+
+    if (counter < 20){
+      setCounter(counter+1)
+    }
+  }
+  const removeValue = () => {
+    if (counter > 0){
+      setCounter(counter-1)
+    }
+    
+  }
   return (
-    <h1> Learning react with vite | Urvashi Kshatriya</h1>
+    <>
+    <div style={{ backgroundColor: "black", color: "white", padding: "20px" }}>
+    <h1>Chai or Code</h1>
+    <h2> Counter Value {counter}</h2>
+    <button onClick={addValue}>Add A number {counter}</button>
+    <button onClick={removeValue}>Remove a number {counter}</button>
+    </div>
+    </>
   )
 }
 
--- basic_react/src/main.jsx
+++ basic_react/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import Chai from './Chai.jsx'
 import App from './App.jsx'
+import Prop from './prop.jsx'
 
 createRoot(document.getElementById('root')).render(
   
     <>
-    <Chai/>
+   
+   <Prop title = 'Urvashi' img = '/urvashi.jpeg'/>
+   
     </>
   
 )
