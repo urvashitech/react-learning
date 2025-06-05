c5ad02ba306a8f9f044ba16a6108c63f64d7c0da
urvashi <urvashikshatriya9@gmail.com>
2025-06-05 23:50:51 +0530

    project setup done

---
+++ 05AppWriteProject/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseUD : String(import.meta.env.VITE_APPWRITE_DATABASEID),
+    appwriteCollectionID : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketID : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf;
