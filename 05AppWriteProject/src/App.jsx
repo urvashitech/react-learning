dafb8a21959ef0ef6b0a64edfbd171db3ef6cb4b
urvashi <urvashikshatriya9@gmail.com>
2025-06-17 15:17:16 +0530

    redux setup in the project

--- 05AppWriteProject/src/App.jsx
+++ 05AppWriteProject/src/App.jsx
@@ -1,14 +1,34 @@
 
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from './component/index'
 import './App.css'
 
 function App() {
   
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>Learning new Thing and making a meha project </h1>
-    </>
-  )
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    authService.getCurrentSession()
+    .then((userData)=>{
+      if (userData){
+        dispatch(login({userData}))
+      }
+      else dispatch(logout())
+    })
+    .finally(()=>{setLoading(false)})
+  },[])
+  return !loading ? (
+     <div className="min-h-screen flex flex-col bg-gray-400 items-center justify-center">
+    <Header />
+    <main className="flex-grow">
+      {/* Your routes/pages or content go here */}
+    </main>
+    <Footer />
+  </div>
+    ) : null
 }
 
 export default App
