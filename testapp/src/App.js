6bfeb59f24e1ab6c41487b6f57aa16c1187d09af
urvashi <urvashikshatriya9@gmail.com>
2025-06-19 21:21:55 +0530

    learned to handle event in react

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -13,12 +13,21 @@ const  Header = ()=>{
 
 }
 function App() {
-  
+  let count = 0 
+  const handleClickIncrement = ()=>{
+    count+=1
+    console.log(count)
+  }
+  const handleTextChange = (e)=>{
+    console.log(e.target.value)
+  }
   const name = "Urvashi"
   return (
     <div className='App'>
       <Header/>
       <h1>Hello {name} i am learning react</h1>
+      <button onClick={handleClickIncrement} style={{background : "blue" , color: "white" , borderRadius : "20px"}}> Increment+</button>
+      <input style={{background : "black" , color: "white" , marginLeft: "10px", marginTop: "10px", borderRadius : "15px"}} onChange={handleTextChange} placeholder='Type anything to print in the console '/>
       
     </div>
   );
