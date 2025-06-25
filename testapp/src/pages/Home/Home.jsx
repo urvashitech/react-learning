b6f4fbddf7104d3f6124b2015a35edf70d4cce92
urvashi <urvashikshatriya9@gmail.com>
2025-06-25 18:53:19 +0530

    Learning programatically routing

--- testapp/src/pages/Home/Home.jsx
+++ testapp/src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+    const navigate = useNavigate()
+    const onCartClick = ()=>{
+        navigate('/cart')
+    }
   return (
     <div>
       <h1>Welcome To Home Page</h1>
+      <button onClick={onCartClick}>Go to cart</button>
     </div>
   )
 }
