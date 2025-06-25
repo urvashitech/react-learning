86440495fe42c6df2780a1a2eb53cd8788e937a2
urvashi <urvashikshatriya9@gmail.com>
2025-06-25 18:47:40 +0530

    Learning about NavLink

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Link , Route, Routes} from 'react-router-dom'
+import { Route, Routes , NavLink} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart';
 import About from './pages/About/About';
@@ -25,6 +25,12 @@ function App() {
     console.log(e.target.value)
   }
   const name = "Urvashi"
+  const getStyle = ({ isActive }) => {
+  return {
+    color: isActive ? "red" : "",
+    margin: "10px"
+  };
+};
   return (
     <div className='App'>
       {/* 
@@ -33,9 +39,9 @@ function App() {
       <button onClick={handleClickIncrement} style={{background : "blue" , color: "white" , borderRadius : "20px"}}> Increment+</button>
       <input style={{background : "black" , color: "white" , marginLeft: "10px", marginTop: "10px", borderRadius : "15px"}} onChange={handleTextChange} placeholder='Type anything to print in the console '/>
       */}
-      <Link to='/' style ={{marginRight : "6px" }}>Home</Link>
-      <Link to='/cart' style ={{marginLeft : "10px"}}>Cart</Link>
-      <Link to='/about' style ={{marginLeft : "10px"}}>About</Link>
+      <NavLink to='/' style ={ getStyle}>Home</NavLink> || 
+      <NavLink to='/cart' style ={ getStyle}>Cart</NavLink> || 
+      <NavLink to='/about' style ={ getStyle}>About</NavLink> ||
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
